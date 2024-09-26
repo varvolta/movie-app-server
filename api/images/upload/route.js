16cdce4c55c7abff0b624ad7a5ca33f3bc26fa76
Vahe Arzumanyan <varvolta@gmail.com>
2024-09-26 12:18:07 +0400

    poster

--- api/images/upload/route.js
+++ api/images/upload/route.js
@@ -3,7 +3,7 @@ const { NextResponse } = require('next/server')
 
 async function POST(request) {
     const { searchParams } = new URL(request.url)
-    const filename = searchParams.get('filename')
+    const filename = searchParams.get('poster')
 
     // ⚠️ The below code is for App Router Route Handlers only
     const blob = await put(filename, request.body, {
