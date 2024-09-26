98c42cd4ba3859d7287b256c71c94e1927a4ebb6
Vahe Arzumanyan <varvolta@gmail.com>
2024-09-26 15:22:02 +0400

    update

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const bodyParser = require('body-parser')
 const movieRoutes = require('./routes/movie.route.js')
 const userRoutes = require('./routes/user.route.js')
 const dotenv = require('dotenv')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
 const cors = require('cors')
+
+const privateKey = fs.readFileSync(path.join(__dirname, 'certs/privkey.pem'), 'utf8')
+const certificate = fs.readFileSync(path.join(__dirname, 'certs/cert.pem'), 'utf8')
+const ca = fs.readFileSync(path.join(__dirname, 'certs/chain.pem'), 'utf8')
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+}
 
 dotenv.config()
 
@@ -25,16 +38,16 @@ mongoose
     .then(() => {
         console.log('Connected to MongoDB')
 
+        const httpsServer = https.createServer(credentials, app)
+        const PORT = process.env.PORT || 3000
+        httpsServer.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
     })
     .catch((err) => {
         console.log('Error connecting to MongoDB:', err)
     })
 
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
 
 module.exports = app
